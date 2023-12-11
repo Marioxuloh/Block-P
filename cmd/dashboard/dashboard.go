4fc1562eab75574c62ffc8abd7c8b8409219ef63
Marioxuloh <mariosan119@gmail.com>
2023-12-11 17:00:24 +0100

    added a build done with react and trying to desplegate

--- cmd/dashboard/dashboard.go
+++ cmd/dashboard/dashboard.go
@@ -1,56 +1,32 @@
 package Dashboard
 
 import (
-	"embed"
-	"html/template"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/markbates/pkger"
 )
 
-//go:embed View/*
-var content embed.FS
-
 func Dashboard(address string) {
-	router := gin.Default()
+	r := gin.Default()
 
-	// Manejo de errores en el middleware
-	router.Use(gin.Recovery())
+	// Cargar el sistema de archivos embebido
+	pkger.Include("/view/build")
 
-	// Configurar ruta para servir archivos estáticos embebidos
-	router.GET("/dashboard/:page?", func(c *gin.Context) {
-		// Obtener el nombre de la página desde la URL
-		page := c.Param("page")
+	// Servir archivos estáticos desde el sistema de archivos embebido
+	r.StaticFS("/static", http.Dir(pkger.Dir("/view/build/static")))
 
-		// Construir la ruta del archivo HTML embebido
-		filePath := "View/" + page + ".html"
+	// Servir el archivo HTML principal
+	r.NoRoute(func(c *gin.Context) {
 
-		// Leer el contenido del archivo HTML desde el sistema de archivos embebido
-		fileContent, err := content.ReadFile(filePath)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error al leer el archivo "+page+".html")
-			return
-		}
+		c.FileFromFS("/view/build/index.html", http.Dir(pkger.Dir("/view")))
 
-		// Crear una plantilla HTML y renderizarla
-		tmpl, err := template.New(page).Parse(string(fileContent))
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error al procesar la plantilla")
-			return
-		}
-
-		// Puedes pasar datos adicionales a la plantilla si es necesario
-		data := map[string]interface{}{
-			// Datos adicionales si es necesario
-		}
-
-		// Renderizar la plantilla en la respuesta HTTP
-		err = tmpl.Execute(c.Writer, data)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error al renderizar la plantilla")
-			return
-		}
 	})
 
-	router.Run(address)
+	// Manejar todas las demás solicitudes que no coinciden con un método HTTP específico
+	r.NoMethod(func(c *gin.Context) {
+		c.FileFromFS("/view/build/index.html", http.Dir(pkger.Dir("/view")))
+	})
+
+	r.Run(address)
 }
