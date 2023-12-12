27ca931e49a129ac96fd3fa8d0427982f3f9b432
Marioxuloh <mariosan119@gmail.com>
2023-12-12 11:49:18 +0100

    se puede desplegar un build de react con mux

--- cmd/dashboard/dashboard.go
+++ cmd/dashboard/dashboard.go
@@ -1,32 +1,71 @@
 package Dashboard
 
 import (
+	"embed"
+	"fmt"
+	"io"
+	"io/fs"
+	"log"
+	"mime"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
-	"github.com/markbates/pkger"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
+//go:embed view/build
+var UI embed.FS
+
 func Dashboard(address string) {
-	r := gin.Default()
 
-	// Cargar el sistema de archivos embebido
-	pkger.Include("/view/build")
+	mux := http.NewServeMux()
 
-	// Servir archivos estáticos desde el sistema de archivos embebido
-	r.StaticFS("/static", http.Dir(pkger.Dir("/view/build/static")))
+	mux.HandleFunc("/", handleStatic)
 
-	// Servir el archivo HTML principal
-	r.NoRoute(func(c *gin.Context) {
+	if err := http.ListenAndServe(address, mux); err != nil {
+		log.Println("server failed:", err)
+	}
+}
+func handleStatic(w http.ResponseWriter, r *http.Request) {
 
-		c.FileFromFS("/view/build/index.html", http.Dir(pkger.Dir("/view")))
+	uiFS, err := fs.Sub(UI, "view/build")
+	if err != nil {
+		log.Fatal("failed to get ui fs", err)
+	}
 
-	})
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
 
-	// Manejar todas las demás solicitudes que no coinciden con un método HTTP específico
-	r.NoMethod(func(c *gin.Context) {
-		c.FileFromFS("/view/build/index.html", http.Dir(pkger.Dir("/view")))
-	})
+	path := filepath.Clean(r.URL.Path)
+	if path == "/" { // Add other paths that you route on the UI side here
+		path = "index.html"
+	}
+	path = strings.TrimPrefix(path, "/")
 
-	r.Run(address)
+	file, err := uiFS.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Println("file", path, "not found:", err)
+			http.NotFound(w, r)
+			return
+		}
+		log.Println("file", path, "cannot be read:", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	contentType := mime.TypeByExtension(filepath.Ext(path))
+	w.Header().Set("Content-Type", contentType)
+	if strings.HasPrefix(path, "static/") {
+		w.Header().Set("Cache-Control", "public, max-age=31536000")
+	}
+	stat, err := file.Stat()
+	if err == nil && stat.Size() > 0 {
+		w.Header().Set("Content-Length", fmt.Sprintf("%d", stat.Size()))
+	}
+
+	n, _ := io.Copy(w, file)
+	log.Println("file", path, "copied", n, "bytes")
 }
