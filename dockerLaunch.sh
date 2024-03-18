42c28f47e60ebb9a541c12895d91f27cac7c811e
Marioxuloh <mariosan119@gmail.com>
2024-03-18 13:39:51 +0100

    launcher dokker done

--- dockerLaunch.sh
+++ dockerLaunch.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+#build imagen dokkerfile
+docker build -t block-p .
+
+#para el contenedor por si acaso lo tenias lanzado
 docker stop block-p-container
 
+#lo borra por si lo tenias lanzado
 docker rm block-p-container
 
+#lo vuelve a crear
 docker run --name block-p-container -d -p 8081:8081 -p 8082:8082 -p 8080:8080 block-p
